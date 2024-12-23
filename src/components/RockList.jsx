c706b3604d12766bf1f78c2b2de51d7a81dd51d2
Keaton Law <keaton.c.law@gmail.com>
2024-12-22 19:05:41 -0600

    added additional jsx for type and user in rocks view

--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -2,13 +2,21 @@ import { useEffect } from "react"
 
 export const RockList = ({ rocks, fetchRocks }) => {
     useEffect(() => {
+        fetchRocks();
     }, [])
 
     const displayRocks = () => {
         if (rocks && rocks.length) {
-            return rocks.map(rock => <div key={`key-${rock.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
-                {rock.name} ({rock.type.label})
-            </div>)
+            return rocks.map(rock => 
+            <div
+             key={`key-${rock.id}`}
+             className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50"
+             >
+                <h2>{rock.name} ({rock.type.label})</h2>
+                <p> weighs {rock.weight} KG </p>
+                <p>In the collection of {rock.user.first_name} {rock.user.last_name}</p>
+            </div>
+            );
         }
 
         return <h3>Loading Rocks...</h3>
